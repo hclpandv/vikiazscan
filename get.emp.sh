b35cdc25eca17bdce414ea0f42e34a77aaa5dcbd
Vikas Pandey <vikaspandey@Vikass-MacBook-Pro.local>
2025-11-23 11:06:22 +0100

    added installation script

---
+++ get.emp.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+CLI_NAME="emp"
+VERSION="v0.1.3"  # you could also fetch latest from GitHub API
+REPO="hclpandv/emp-cli"
+
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+# Map architecture
+case $ARCH in
+    x86_64) ARCH="amd64" ;;
+    arm64|aarch64) ARCH="arm64" ;;
+    *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+esac
+
+# download URL
+URL="https://raw.githubusercontent.com/hclpandv/vikiazscan/refs/heads/main/emp-linux-amd64"
+
+# Download binary
+TMP_FILE=$(mktemp)
+echo "Downloading $URL ..."
+curl -L -o "$TMP_FILE" "$URL"
+
+# Make executable
+chmod +x "$TMP_FILE"
+
+# Install to /usr/local/bin
+INSTALL_DIR="/usr/local/bin"
+sudo mv "$TMP_FILE" "$INSTALL_DIR/$CLI_NAME"
+
+echo "$CLI_NAME installed to $INSTALL_DIR/$CLI_NAME"
+echo "Run '$CLI_NAME --help' to get started."
